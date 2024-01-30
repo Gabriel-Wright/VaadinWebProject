9f564876395423bf15066f5b735fd9be5e4fae95
Gabriel Wright <gabemagwright@gmail.com>
2024-01-30 14:08:40 +0000

    full initial commit

---
+++ src/main/java/com/gabeWebTest/webTest/Application.java
@@ -0,0 +1,16 @@
+package com.gabeWebTest.webTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point of our sprint boot application.
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
