54321a2d167dd5a48dfd8d9124b428e516345b39
Gabriel Wright <gabemagwright@gmail.com>
2024-02-07 17:16:26 +0000

    extra integration tests and searches with webrepository

--- src/main/java/com/gabeWebTest/webTest/data/WebPageRepository.java
+++ src/main/java/com/gabeWebTest/webTest/data/WebPageRepository.java
@@ -8,14 +8,23 @@ import java.util.List;
 
 public interface WebPageRepository extends JpaRepository<WebPage, Long> {
 
-    //Finds all webpages ordered by time created
+    //Finds all webpages ordered by time created descending
     List<WebPage> findAllByOrderByDates_TimeCreatedDesc();
+
+    //Find all webpages ordered by time created ascending
+    List<WebPage> findAllByOrderByDates_TimeCreatedAsc();
 
     //Finds all tags with certain tags
     List<WebPage> findByTagsIn(List<Tag> tags);
 
+    //Filters by tags in order of date created descending
     List<WebPage> findByTagsInOrderByDatesTimeCreatedDesc(List<Tag> tags);
 
-    List<WebPage> findByDatesTimeCreatedBetweenAndTagsIn(LocalDate startDate, LocalDate endDate, List<Tag> tags);
+    //Filters by tags in order of date created ascending
+    List<WebPage> findByTagsInOrderByDatesTimeCreatedAsc(List<Tag> tags);
+
+//    //Filter by webpages by tags in between a certain date range by date created descending
+//    List<WebPage> findByDatesTimeCreatedBetweenAndTagsInOrderByDatesTimeCreatedDesc(LocalDateTime startDate, LocalDateTime endDate, List<Tag> tags);
+
 
 }
--- src/test/java/com/gabeWebTest/webTest/integrationTest/WebPageServiceIntegrationTest.java
+++ src/test/java/com/gabeWebTest/webTest/integrationTest/WebPageServiceIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -19,7 +20,6 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Transactional //ensures each test method runs within a transaction, and changes are rolled back after each test method
 @ActiveProfiles("test")
 public class WebPageServiceIntegrationTest {
 
@@ -151,4 +151,134 @@ public class WebPageServiceIntegrationTest {
 
         assertFalse(webPagesWithTag1.contains(webPage3));
     }
+
+    @Test
+    void testFindWebPagesByTagsOrderByDateCreatedDesc() {
+        // Clear existing data
+        webPageRepository.deleteAll();
+        articleFormatRepository.deleteAll();
+        tagRepository.deleteAll();
+
+        // Article formats
+        ArticleFormat articleFormat1 = new ArticleFormat(0L, "test1");
+        ArticleFormat articleFormat2 = new ArticleFormat(1L, "test2");
+        articleFormatRepository.save(articleFormat1);
+        articleFormatRepository.save(articleFormat2);
+
+        // Tag formats
+        Tag tag1 = new Tag(0, "tag1");
+        Tag tag2 = new Tag(1, "tag2");
+        Tag tag3 = new Tag(2, "tag3");
+
+        tagRepository.save(tag1);
+        tagRepository.save(tag2);
+        tagRepository.save(tag3);
+
+        // Web pages
+        Set<Tag> tagSet1 = new HashSet<>();
+        tagSet1.add(tag1);
+        Dates webPage1Dates = new Dates(LocalDateTime.of(2023, 1, 1, 10, 0, 0),
+                LocalDateTime.of(2023, 1, 1, 10, 0, 0));
+        WebPage webPage1 = new WebPage(1L,0,"webpage1",new ArticleText(),null,webPage1Dates, tagSet1, articleFormat1);
+
+        Set<Tag> tagSet2 = new HashSet<>();
+        tagSet2.add(tag1);
+        tagSet2.add(tag2);
+        Dates webPage2Dates = new Dates(LocalDateTime.of(2023, 1, 2, 10, 0,0), LocalDateTime.of(2023,1,2,10,0,0));
+        WebPage webPage2 = new WebPage(2L, 0, "webpage2", new ArticleText(), null, webPage2Dates, tagSet2, articleFormat2);
+
+        Set<Tag> tagSet3 = new HashSet<>();
+        tagSet3.add(tag3);
+        Dates webPage3Dates = new Dates(LocalDateTime.of(2023, 1, 3, 10, 0,0), LocalDateTime.of(2023,1,2,10,0,0));
+        WebPage webPage3 = new WebPage(3L, 0, "webpage3", new ArticleText(), null, webPage3Dates, tagSet3, articleFormat1);
+
+        webPageRepository.save(webPage1);
+        webPageRepository.save(webPage2);
+        webPageRepository.save(webPage3);
+
+        // Create a list of tags
+        List<Tag> tagList1 = new ArrayList<>();
+        tagList1.add(tag1);
+
+        // Find web pages by tags and order by date created descending
+        List<WebPage> webPagesWithTag1OrderedDesc = webPageRepository.findByTagsInOrderByDatesTimeCreatedDesc(tagList1);
+
+        // Assertions
+        assertTrue(webPagesWithTag1OrderedDesc.contains(webPage1));
+        assertTrue(webPagesWithTag1OrderedDesc.contains(webPage2));
+        assertEquals(2, webPagesWithTag1OrderedDesc.size());
+
+        // Test for the ordering
+        LocalDateTime latestDate = webPagesWithTag1OrderedDesc.get(0).getDates().getTimeCreated();
+        System.out.println(latestDate);
+        LocalDateTime earlierDate = webPagesWithTag1OrderedDesc.get(1).getDates().getTimeCreated();
+        System.out.println(earlierDate);
+        Assertions.assertTrue(latestDate.isAfter(earlierDate), "Checking latestDate after earlier one");
+    }
+
+    // Test for findByDatesTimeCreatedBetweenAndTagsInOrderByDatesTimeCreatedDesc method
+//    @Test
+//    void testFindWebPagesByDateAndTagsOrderByDateCreatedDesc() {
+//        // Clear existing data
+//        webPageRepository.deleteAll();
+//        articleFormatRepository.deleteAll();
+//        tagRepository.deleteAll();
+//
+//        // Article formats
+//        ArticleFormat articleFormat1 = new ArticleFormat(0L, "test1");
+//        ArticleFormat articleFormat2 = new ArticleFormat(1L, "test2");
+//        articleFormatRepository.save(articleFormat1);
+//        articleFormatRepository.save(articleFormat2);
+//
+//        // Tag formats
+//        Tag tag1 = new Tag(0, "tag1");
+//        Tag tag2 = new Tag(1, "tag2");
+//        Tag tag3 = new Tag(2, "tag3");
+//
+//        tagRepository.save(tag1);
+//        tagRepository.save(tag2);
+//        tagRepository.save(tag3);
+//
+//        // Web pages
+//        Set<Tag> tagSet1 = new HashSet<>();
+//        tagSet1.add(tag1);
+//        Dates webPage1Dates = new Dates(LocalDateTime.of(2023, 1, 1, 10, 0, 0),
+//                LocalDateTime.of(2023, 1, 1, 10, 0, 0));
+//        WebPage webPage1 = new WebPage(1L,0,"webpage1",new ArticleText(),null,webPage1Dates, tagSet1, articleFormat1);
+//
+//        Set<Tag> tagSet2 = new HashSet<>();
+//        tagSet2.add(tag1);
+//        tagSet2.add(tag2);
+//        Dates webPage2Dates = new Dates(LocalDateTime.of(2023, 1, 2, 10, 0,0), LocalDateTime.of(2023,1,2,10,0,0));
+//        WebPage webPage2 = new WebPage(2L, 0, "webpage2", new ArticleText(), null, webPage2Dates, tagSet2, articleFormat2);
+//
+//        Set<Tag> tagSet3 = new HashSet<>();
+//        tagSet3.add(tag3);
+//        Dates webPage3Dates = new Dates(LocalDateTime.of(2023, 1, 3, 10, 0,0), LocalDateTime.of(2023,1,2,10,0,0));
+//        WebPage webPage3 = new WebPage(3L, 0, "webpage3", new ArticleText(), null, webPage3Dates, tagSet3, articleFormat1);
+//
+//        webPageRepository.save(webPage1);
+//        webPageRepository.save(webPage2);
+//        webPageRepository.save(webPage3);
+//
+//        // Create a list of tags
+//        List<Tag> tagList1 = new ArrayList<>();
+//        tagList1.add(tag1);
+//
+//        // Find web pages by date range and tags, order by date created descending
+//        LocalDateTime startDate = LocalDateTime.of(2022, 12, 31,9,0,0);
+//        LocalDateTime endDate = LocalDateTime.of(2023, 1, 3,9,0,0);
+//        List<WebPage> webPagesBetweenDateAndTagsOrderedDesc = webPageRepository.findByDatesTimeCreatedBetweenAndTagsInOrderByDatesTimeCreatedDesc(startDate, endDate, tagList1);
+//
+//        System.out.println(webPagesBetweenDateAndTagsOrderedDesc.size());
+//        // Assertions
+//        assertTrue(webPagesBetweenDateAndTagsOrderedDesc.contains(webPage1));
+//        assertFalse(webPagesBetweenDateAndTagsOrderedDesc.contains(webPage3)); // webPage3 has a date outside the range
+//        assertEquals(1, webPagesBetweenDateAndTagsOrderedDesc.size());
+//
+//        // Test for the ordering
+//        LocalDateTime latestDate = webPagesBetweenDateAndTagsOrderedDesc.get(0).getDates().getTimeCreated();
+//        assertEquals(LocalDateTime.of(2023, 1, 1, 10, 0, 0), latestDate);
+//    }
+
 }
