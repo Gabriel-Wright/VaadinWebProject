85c1a13b65b78fb6032953054f6068cb3b3035e7
Gabriel Wright <gabemagwright@gmail.com>
2024-02-11 19:27:31 +0000

    Autowiring navigation bar

--- src/main/java/com/gabeWebTest/webTest/views/dashboard/DashboardView.java
+++ src/main/java/com/gabeWebTest/webTest/views/dashboard/DashboardView.java
@@ -1,12 +1,9 @@
 package com.gabeWebTest.webTest.views.dashboard;
 
-import com.gabeWebTest.webTest.data.Tag;
-import com.gabeWebTest.webTest.services.TagService;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.combobox.MultiSelectComboBox;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
@@ -14,16 +11,10 @@ import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Route("")
 @PageTitle("Dashboard")
@@ -31,17 +22,14 @@ public class DashboardView extends AppLayout {
 
     private H1 viewTitle;
     private final TagFilter tagFilter;
+    private final NavigationBar navigationBar;
 
     @Autowired
-    public DashboardView(TagFilter tagFilter) {
+    public DashboardView(TagFilter tagFilter, NavigationBar navigationBar) {
         this.tagFilter = tagFilter;
+        this.navigationBar = navigationBar;
         DrawerToggle toggle = new DrawerToggle();
-        addToNavbar(createNavBarContent(toggle));
-        SideNav nav = getSideNav();
-
-//        Scroller scroller = new Scroller(nav);
-//        scroller.setClassName(LumoUtility.Padding.SMALL);
-
+        addToNavbar(navigationBar.createNavBarContent(toggle));
         addToDrawer(new SideNavItem("Home", "/",
                 VaadinIcon.HOME.create()));
         addToDrawer(new SideNavItem("Random article","/",
@@ -56,48 +44,39 @@ public class DashboardView extends AppLayout {
         setContent(testButton);
     }
 
-    private SideNav getSideNav() {
-        SideNav nav = new SideNav();
-        nav.addItem(new SideNavItem("Home", "/",
-                VaadinIcon.HOME.create()));
-        nav.addItem(new SideNavItem("Random article","/",
-                        VaadinIcon.RANDOM.create()));
-        return nav;
-    }
-
-    private Component createNavBarContent(DrawerToggle drawerToggle) {
-        HorizontalLayout layout = new HorizontalLayout();
-        layout.setId("header");
-        layout.setAlignItems(FlexComponent.Alignment.CENTER);
-        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
-        layout.setWidthFull();
-
-
-        // Add the drawer toggle to the layout
-        layout.add(drawerToggle);
-
-//        // Add the view title to the layout
-//        viewTitle = new H1("Your Title"); // Replace "Your Title" with the actual title
-//        layout.add(viewTitle);
-
-
-        // Add the LinkedIn image and link
-        Image linkedInImage = new Image("img/LI-In-Bug.png", "LinkedIn");
-        linkedInImage.addClassName("responsive-logo");
-        linkedInImage.getElement().getStyle().set("max-width", "100%");
-        linkedInImage.getElement().getStyle().set("height", "auto");
-        Anchor linkedinLinkImage = new Anchor("https://www.linkedin.com/in/gabriel-wright-127b0724b/", linkedInImage);
-//        Div linkedinDiv = new Div(linkedinLinkImage);
-//        // Adjust margins and padding for the Div
-//        linkedinDiv.getElement().getStyle().set("margin", "0");
-//        linkedinDiv.getElement().getStyle().set("padding", "0");
-
-
-        layout.add(linkedinLinkImage);
-
-
-        return layout;
-    }
+//    private Component createNavBarContent(DrawerToggle drawerToggle) {
+//        HorizontalLayout layout = new HorizontalLayout();
+//        layout.setId("header");
+//        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+//        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+//        layout.setWidthFull();
+//
+//
+//        // Add the drawer toggle to the layout
+//        layout.add(drawerToggle);
+//
+////        // Add the view title to the layout
+////        viewTitle = new H1("Your Title"); // Replace "Your Title" with the actual title
+////        layout.add(viewTitle);
+//
+//
+//        // Add the LinkedIn image and link
+//        Image linkedInImage = new Image("img/LI-In-Bug.png", "LinkedIn");
+//        linkedInImage.addClassName("responsive-logo");
+//        linkedInImage.getElement().getStyle().set("max-width", "100%");
+//        linkedInImage.getElement().getStyle().set("height", "auto");
+//        Anchor linkedinLinkImage = new Anchor("https://www.linkedin.com/in/gabriel-wright-127b0724b/", linkedInImage);
+////        Div linkedinDiv = new Div(linkedinLinkImage);
+////        // Adjust margins and padding for the Div
+////        linkedinDiv.getElement().getStyle().set("margin", "0");
+////        linkedinDiv.getElement().getStyle().set("padding", "0");
+//
+//
+//        layout.add(linkedinLinkImage);
+//
+//
+//        return layout;
+//    }
 
 //    private void createTagFilterDropDown() {
 //        // Get all tags from TagService
--- src/main/java/com/gabeWebTest/webTest/views/dashboard/NavigationBar.java
+++ src/main/java/com/gabeWebTest/webTest/views/dashboard/NavigationBar.java
@@ -1,4 +1,42 @@
 package com.gabeWebTest.webTest.views.dashboard;
 
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NavigationBar {
+
+    private final String linkedINImagePath = "img/LI-In-Bug.png";
+    private final String linkedINLink = "https://www.linkedin.com/in/gabriel-wright-127b0724b/";
+
+    public HorizontalLayout createNavBarContent(DrawerToggle drawerToggle) {
+        HorizontalLayout layout = new HorizontalLayout();
+        layout.setId("header");
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+        layout.setWidthFull();
+
+        // Add the drawer toggle to the layout
+        layout.add(drawerToggle);
+
+        Anchor linkedINAnchor = createLinkedInAnchor();
+
+        layout.add(linkedINAnchor);
+
+        return layout;
+    }
+
+    private Anchor createLinkedInAnchor() {
+        // Add the LinkedIn image and link
+        Image linkedInImage = new Image(linkedINImagePath, "LinkedIn");
+        linkedInImage.addClassName("responsive-logo");
+        linkedInImage.getElement().getStyle().set("max-width", "100%");
+        linkedInImage.getElement().getStyle().set("height", "auto");
+        return new Anchor(this.linkedINLink, linkedInImage);
+    }
+
 }
