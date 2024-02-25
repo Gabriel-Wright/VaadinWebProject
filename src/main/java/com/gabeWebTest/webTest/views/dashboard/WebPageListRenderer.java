1f843d1611194e5b6a3689ba67c70c2d5a7d98d9
Gabriel Wright <gabemagwright@gmail.com>
2024-02-25 19:30:35 +0000

    Font adjustments, loadable from server

--- src/main/java/com/gabeWebTest/webTest/views/dashboard/WebPageListRenderer.java
+++ src/main/java/com/gabeWebTest/webTest/views/dashboard/WebPageListRenderer.java
@@ -48,33 +48,29 @@ public class WebPageListRenderer extends ComponentRenderer<HorizontalLayout, Web
         Paragraph paragraph = new Paragraph("This is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThisThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThisThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThisThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please work");
         paragraph.addClassName("article-preview-text");
         paragraph.setWidthFull();
+        HorizontalLayout tagComponent = createTagDisplayComponent(item);
+        tagComponent.setAlignItems(FlexComponent.Alignment.END);
         verticalText.add(componentTitle, paragraph, createTagDisplayComponent(item));
         verticalText.setPadding(false);
         verticalText.setSpacing(false);
-        verticalText.setHeightFull();
-        componentTitle.setHeight("30%");
-        paragraph.setHeight("10%");
-        verticalText.setAlignItems(FlexComponent.Alignment.CENTER);
+//        componentTitle.setHeight("30%");
+//        paragraph.setHeight("10%");
+        verticalText.setAlignItems(FlexComponent.Alignment.START);
         verticalText.addClassName("article-style");
-//        // Set alignment to top for the VerticalLayout
-//         verticalText.setAlignItems(FlexComponent.Alignment.START);
         //Publication date and tags
         Text publicationText = new Text("Published on" + item.getDates().getTimeCreated());
-        //Paragraph articleText = new Paragraph(item.getArticleText().getArticleText());
         // Create a vertical layout to contain the components
         HorizontalLayout layout = new HorizontalLayout(verticalText, image);
-        // verticalText.setWidth("70%"); // Ensure the layout takes up the available width
         layout.setPadding(false);
         layout.setSpacing(false);
         layout.setWidthFull();
-//        image.setWidth("67%");
-//        image.getStyle().set("height",image.getHeight());
-//        image.getStyle().set("width",image.getWidth());
+        layout.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
         verticalText.setWidth("30%");
-        layout.setAlignItems(FlexComponent.Alignment.START); // Align items to the top
-
-        // Set layout width to fill the available space
-        // layout.setVerticalComponentAlignment(FlexComponent.Alignment.START); // Center-align items vertically
+//        layout.setAlignItems(FlexComponent.Alignment.START); // Align items to the top
+//
+//        // Set layout width to fill the available space
+//        // layout.setVerticalComponentAlignment(FlexComponent.Alignment.START); // Center-align items vertically
 
         // Add a CSS class for styling
         layout.addClassName("article-style");
@@ -83,12 +79,12 @@ public class WebPageListRenderer extends ComponentRenderer<HorizontalLayout, Web
     }
 
     @Transactional
-    private Component createTagDisplayComponent(WebPage item) {
+    private HorizontalLayout createTagDisplayComponent(WebPage item) {
         Set<Tag> tags = item.getTags();
         HorizontalLayout horizontalLayout = new HorizontalLayout();
         //Add border and rounded corners
-        horizontalLayout.setWidth("20%"); // Ensure it takes up only the required space
-        horizontalLayout.setHeight("20%");
+//        horizontalLayout.setWidth("20%"); // Ensure it takes up only the required space
+//        horizontalLayout.setHeight("20%");
         horizontalLayout.setPadding(false); // Disable padding to minimize extra space
 
         for(Tag tag: tags) {
