81bfc7891a0eacd89b77fda3055e3f08f743e34a
Gabriel Wright <gabemagwright@gmail.com>
2024-02-28 15:20:54 +0000

    Organising WebPageListRenderer to make code more readable

--- src/main/java/com/gabeWebTest/webTest/views/dashboard/WebPageListRenderer.java
+++ src/main/java/com/gabeWebTest/webTest/views/dashboard/WebPageListRenderer.java
@@ -20,6 +20,8 @@ import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.function.SerializableSupplier;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Set;
 
 public class WebPageListRenderer extends ComponentRenderer<HorizontalLayout, WebPage> {
@@ -31,40 +33,9 @@ public class WebPageListRenderer extends ComponentRenderer<HorizontalLayout, Web
     @Override
     public HorizontalLayout createComponent(WebPage item) {
         // Create components to represent a single item
-        Image image;
-        if(item.getThumbnailPath()==null) {
-            image = new Image("img/LI-In-Bug.png", "ArticleImage");
-        } else {
-            image = new Image(item.getThumbnailPath(),item.getTitle());
-        }
-        image.addClassName("image-container");
+        Image image = loadWebPagePreviewImage(item);
+        VerticalLayout verticalText = loadWebPageVerticalText(item);
 
-//        image.setWidth("70%"); // Set image width to 70% of parent container
-//        image.setHeight("auto"); // Maintain aspect ratio
-
-        VerticalLayout verticalText = new VerticalLayout();
-        // You can add more components here to display other details of the web page
-        H2 componentTitle = new H2(item.getTitle());
-        componentTitle.addClassName("adaptive-title-font");
-//        componentTitle.setWidthFull();
-
-        Paragraph paragraph = new Paragraph("This is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThisThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThisThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThisThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please work");
-        paragraph.addClassName("article-preview-text");
-//        paragraph.setHeight("3em");
-//        paragraph.setWidthFull();
-        HorizontalLayout tagComponent = createTagDisplayComponent(item);
-        tagComponent.setAlignItems(FlexComponent.Alignment.START);
-        tagComponent.setJustifyContentMode(FlexComponent.JustifyContentMode.START);
-        verticalText.add(componentTitle, paragraph, tagComponent);
-        verticalText.setPadding(false);
-        verticalText.setSpacing(false);
-        verticalText.setSizeFull();
-//        componentTitle.setHeight("30%");
-//        paragraph.setHeight("10%");
-//        verticalText.addClassName("article-style");
-        //Publication date and tags
-        Text publicationText = new Text("Published on" + item.getDates().getTimeCreated());
-        // Create a vertical layout to contain the components
         HorizontalLayout layout = new HorizontalLayout(verticalText, image);
         layout.setPadding(false);
         layout.setSpacing(false);
@@ -72,15 +43,8 @@ public class WebPageListRenderer extends ComponentRenderer<HorizontalLayout, Web
 
         layout.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
         layout.setAlignItems(FlexComponent.Alignment.START);
-        verticalText.setWidth("30%");
-//        verticalText.setWidthFull();
         layout.expand(verticalText);
-//        layout.setAlignItems(FlexComponent.Alignment.START); // Align items to the top
-//
-//        // Set layout width to fill the available space
-//        // layout.setVerticalComponentAlignment(FlexComponent.Alignment.START); // Center-align items vertically
 
-        // Add a CSS class for styling
         layout.addClassName("article-style");
 
         return layout;
@@ -95,10 +59,74 @@ public class WebPageListRenderer extends ComponentRenderer<HorizontalLayout, Web
 
         for(Tag tag: tags) {
             horizontalLayout.add(tag.createTagComponent());
-            horizontalLayout.add(tag.createTagComponent());
-            horizontalLayout.add(tag.createTagComponent());
         }
         return horizontalLayout;
     }
 
+    private Image loadWebPagePreviewImage(WebPage item) {
+        Image image;
+        if(item.getThumbnailPath()==null) {
+            image = new Image("img/LI-In-Bug.png", "ArticleImage");
+        } else {
+            image = new Image(item.getThumbnailPath(),item.getTitle());
+        }
+        image.addClassName("image-container");
+        return image;
+    }
+
+    private VerticalLayout loadWebPageVerticalText(WebPage item) {
+        VerticalLayout verticalText = new VerticalLayout();
+        H2 componentTitle = loadWebpPagePreviewTitle(item);
+
+        Paragraph paragraph = loadWebpagePreviewParagraph(item);
+        HorizontalLayout tagComponent = loadWebPageTags(item);
+        Span articleDate = loadWebPageDate(item);
+
+        verticalText.add(componentTitle, articleDate,paragraph, tagComponent);
+        verticalText.setPadding(false);
+        verticalText.setSpacing(false);
+        verticalText.setSizeFull();
+        verticalText.setWidth("30%");
+
+        return verticalText;
+    }
+
+    private H2 loadWebpPagePreviewTitle(WebPage item) {
+        H2 componentTitle = new H2(item.getTitle());
+        componentTitle.addClassName("adaptive-title-font");
+        return componentTitle;
+    }
+
+    private Paragraph loadWebpagePreviewParagraph(WebPage item) {
+        Paragraph paragraph = new Paragraph("This is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThisThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThisThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThisThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please workThis is preview text dear god please work");
+        paragraph.addClassName("article-preview-text");
+
+        return paragraph;
+    }
+
+    private HorizontalLayout loadWebPageTags(WebPage item) {
+        HorizontalLayout tagsComponent = createTagDisplayComponent(item);
+        tagsComponent.setAlignItems(FlexComponent.Alignment.START);
+        tagsComponent.setJustifyContentMode(FlexComponent.JustifyContentMode.START);
+
+        return tagsComponent;
+    }
+
+    private Span loadWebPageDate(WebPage item) {
+        LocalDate webPageCreationDate = item.getDates().getTimeCreated().toLocalDate();
+        LocalTime webPageCreationTime = item.getDates().getTimeCreated().toLocalTime();
+        LocalDate webPageLastUpdateDate = item.getDates().getTimeLastUpdated().toLocalDate();
+        LocalTime webPageLastUpdateTime = item.getDates().getTimeLastUpdated().toLocalTime();
+
+        Span articleDate = new Span(webPageCreationDate.toString());
+        articleDate.addClassName("date-style");
+
+        String dateTimeCreated = "Article created:" +webPageCreationDate.toString() +" " +webPageCreationTime.toString();
+        String dateLastUpdated = "Article last Updated:" + webPageLastUpdateDate.toString() + " " + webPageLastUpdateTime.toString();
+        Tooltip dateTip = Tooltip.forComponent(articleDate).
+                withText(dateTimeCreated +"\n"+dateLastUpdated).
+                withPosition(Tooltip.TooltipPosition.BOTTOM);
+
+        return articleDate;
+    }
 }
