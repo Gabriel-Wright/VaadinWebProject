38ca6a6ecc659fbbd5426de04074c0b8ba5f20b7
Gabriel Wright <gabemagwright@gmail.com>
2024-04-03 15:28:59 +0100

    Education writeup - need to refactor to make a bit prettier / more easily editable , but it isnt too bad

--- src/main/java/com/gabeWebTest/webTest/views/AboutMeView.java
+++ src/main/java/com/gabeWebTest/webTest/views/AboutMeView.java
@@ -7,10 +7,7 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -103,11 +100,37 @@ public class AboutMeView extends AppLayout {
         VerticalLayout titleAndTextBlock = new VerticalLayout();
         H1 title = new H1("Educational Background");
         title.addClassName("about-me-title");
-        String educationText = "";
-        Paragraph paragraph = new Paragraph();
-        paragraph.add(educationText);
-        paragraph.addClassName("about-me-text");
-        titleAndTextBlock.add(title, paragraph);
+        String educationText1 = "I completed my Undergraduate degree at the University of Warwick between September 2018 and July 2021. Initially, I pursued a joint degree in Maths and Philosophy, "+
+               "due to my split interest in both subjects and their interconnectedness. Particularly intriguing to me was the exploration of mathematical logic and its implications on metaphysical concepts of truth.";
+        String educationText2 = "During my 2nd year I transitioned to a full BSc in Mathematics. This shift stemmed from a realization that Mathematics offered a more tangible avenue for academic pursuit and growth. "+
+                "While I valued and was engaged by the philosophy discussion in lectures, I found the structured framework of the Mathematics degree suited me better. "+
+                "In the later years of my degree I specialised within Mathematical Analysis and Abstract Algebra.";
+        String educationText3 = "Following my undergraduate degree I started on a master's program between October 2021 and September 2022. During the summer of 2021, before the start of my next academic year, I found myself increasingly drawn to the fields of computer science and artificial intelligence. " +
+                "Recognizing the growing importance and applications of these disciplines, I decided to pivot towards a more hands-on researched based MSc in Data Analytics. "+
+                "Throughout my MSc program, I learnt various data analytic techniques including but not limited to: .";
+        Paragraph paragraph1 = new Paragraph();
+        Paragraph paragraph2 = new Paragraph();
+        Paragraph paragraph3 = new Paragraph();
+        Paragraph paragraph4 = new Paragraph();
+
+        paragraph1.add(educationText1);
+        paragraph1.addClassName("about-me-text");
+        paragraph2.add(educationText2);
+        paragraph2.addClassName("about-me-text");
+        paragraph3.add(educationText3);
+        paragraph3.addClassName("about-me-text");
+        titleAndTextBlock.add(title, paragraph1, paragraph2, paragraph3);
+        Span bullet1 = new Span("• Data pre-processing and cleaning techniques.");
+        Span bullet2 = new Span("• Machine learning algorithms such as regression, classification, clustering and neural networks.");
+        Span bullet3 = new Span("• Statistical modelling, including hypothesis testing and utilising probabilistic modelling techniques to predict future outcomes.");
+        Span bullet4 = new Span("• Current applications and implementations of Natural Language Processing, such as in Sentiment Analysis, Information Extraction and Recommender Systems.");
+        Span bullet5 = new Span("• Algorithmic Game Theory approaches to solving game theoretic problems efficiently.");
+        bullet1.addClassName("about-me-text");
+        bullet2.addClassName("about-me-text");
+        bullet3.addClassName("about-me-text");
+        bullet4.addClassName("about-me-text");
+        bullet5.addClassName("about-me-text");
+        titleAndTextBlock.add(bullet1, bullet2, bullet3, bullet4, bullet5);
         return titleAndTextBlock;
     }
 
