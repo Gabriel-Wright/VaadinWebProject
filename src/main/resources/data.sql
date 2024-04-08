c2d86b8ce76e2f63683e48a22988ca820d79a8a0
Gabriel Wright <gabemagwright@gmail.com>
2024-04-08 20:03:09 +0100

    Switched over to mySQL. Now should be able to better diagnose jwt issue. Need to change article text storing method

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,51 +1,49 @@
-INSERT INTO "USERS" (ID, USERNAME, PASSWORD, ROLE) VALUES (-1, 'GMW', '$2a$12$lOfOaW4b5ngvxdsKnQCy5umEgXWZW2/Is8Xmzsb5laX0sh7RRXpJa', 'ADMIN');
+INSERT INTO USERS (ID, USERNAME, PASSWORD, ROLE) VALUES (1, 'GMW', '$2a$12$lOfOaW4b5ngvxdsKnQCy5umEgXWZW2/Is8Xmzsb5laX0sh7RRXpJa', 'ADMIN');
 
-INSERT INTO "TAG" (ID, TAGNAME, COLOR_HEX, ICON) VALUES (1, 'Gaming', '#ff0000', 'GAMEPAD');
-INSERT INTO "TAG" (ID, TAGNAME, COLOR_HEX, ICON) VALUES (2, 'Travel', '#edd000', 'FLIGHT_TAKEOFF');
-INSERT INTO "TAG" (ID, TAGNAME, COLOR_HEX, ICON) VALUES (3, 'Art', '#3deeed', 'PAINTBRUSH');
+INSERT INTO TAG (ID, TAGNAME, COLOR_HEX, ICON) VALUES (1, 'Gaming', '#ff0000', 'GAMEPAD');
+INSERT INTO TAG (ID, TAGNAME, COLOR_HEX, ICON) VALUES (2, 'Travel', '#edd000', 'FLIGHT_TAKEOFF');
+INSERT INTO TAG (ID, TAGNAME, COLOR_HEX, ICON) VALUES (3, 'Art', '#3deeed', 'PAINTBRUSH');
 -- Insert sample tags
-INSERT INTO "TAG" (ID, TAGNAME, COLOR_HEX, ICON) VALUES (-1, 'Tag1', '#ff0000', 'EYE');
-INSERT INTO "TAG" (ID, TAGNAME, COLOR_HEX, ICON) VALUES (-2, 'Tag2', '#ff0000', 'EYE');
-INSERT INTO "TAG" (ID, TAGNAME, COLOR_HEX, ICON) VALUES (-3, 'Tag3', '#ff0000', 'EYE');
+INSERT INTO TAG (ID, TAGNAME, COLOR_HEX, ICON) VALUES (-1, 'Tag1', '#ff0000', 'EYE');
+INSERT INTO TAG (ID, TAGNAME, COLOR_HEX, ICON) VALUES (-2, 'Tag2', '#ff0000', 'EYE');
+INSERT INTO TAG (ID, TAGNAME, COLOR_HEX, ICON) VALUES (-3, 'Tag3', '#ff0000', 'EYE');
 
 -- Insert sample article formats
-INSERT INTO "ARTICLE_FORMAT" (ARTICLE_FORMAT_ID, NAME) VALUES (-1, 'Format A');
-INSERT INTO "ARTICLE_FORMAT" (ARTICLE_FORMAT_ID, NAME) VALUES (-2, 'Format B');
+INSERT INTO ARTICLE_FORMAT (ARTICLE_FORMAT_ID, NAME) VALUES (1, 'Format A');
+INSERT INTO ARTICLE_FORMAT (ARTICLE_FORMAT_ID, NAME) VALUES (2, 'Format B');
 
 -- Insert sample web pages
-INSERT INTO "WEB_PAGE" (ID, VERSION, TITLE, ARTICLE_FORMAT_ID) VALUES (-1000, 0, 'Memories Of Murder: Analysis and Ending explained', -1);
-INSERT INTO "WEB_PAGE" (ID, VERSION, TITLE, ARTICLE_FORMAT_ID) VALUES (-1001, 0, 'JFrame Game Development', -1);
-INSERT INTO "WEB_PAGE" (ID, VERSION, TITLE, ARTICLE_FORMAT_ID) VALUES (-1002, 0, 'A.I Pathfinding approaches', -1);
---INSERT INTO "WEB_PAGE" (ID, VERSION, TITLE, ARTICLE_FORMAT_ID) VALUES (-1001, 0, 'Web Page 2', -1);
---INSERT INTO "WEB_PAGE" (ID, VERSION, TITLE, ARTICLE_FORMAT_ID) VALUES (-1002, 0, 'Web Page 3', -2);
+INSERT INTO WEB_PAGE (ID, VERSION, TITLE, ARTICLE_FORMAT_ID) VALUES (1000, 0, 'Memories Of Murder: Analysis and Ending explained', 1);
+INSERT INTO WEB_PAGE (ID, VERSION, TITLE, ARTICLE_FORMAT_ID) VALUES (1001, 0, 'JFrame Game Development', 1);
+INSERT INTO WEB_PAGE (ID, VERSION, TITLE, ARTICLE_FORMAT_ID) VALUES (1002, 0, 'A.I Pathfinding approaches', 1);
 
 -- Insert sample dates for the web pages
-UPDATE "WEB_PAGE" SET TIME_CREATED = '2023-01-01T10:00:00', TIME_LAST_UPDATED = '2023-01-01T10:00:00' WHERE id = -1000;
-UPDATE "WEB_PAGE" SET TIME_CREATED = '2024-08-03T18:49:00', TIME_LAST_UPDATED = '2024-08-03T18:49:00' where id = -1001;
-UPDATE "WEB_PAGE" SET TIME_CREATED = '2024-02-15T15:23:00', TIME_LAST_UPDATED = '2024-02-15T15:23:00' where id = -1002;
---UPDATE "WEB_PAGE" SET TIME_CREATED = '2023-01-02T10:00:00', TIME_LAST_UPDATED = '2023-01-02T10:00:00' WHERE id = -1001;
---UPDATE "WEB_PAGE" SET TIME_CREATED = '2023-01-03T10:00:00', TIME_LAST_UPDATED = '2023-01-03T10:00:00' WHERE id = -1002;
+UPDATE WEB_PAGE SET TIME_CREATED = '2023-01-01T10:00:00', TIME_LAST_UPDATED = '2023-01-01T10:00:00' WHERE id = 1000;
+UPDATE WEB_PAGE SET TIME_CREATED = '2024-08-03T18:49:00', TIME_LAST_UPDATED = '2024-08-03T18:49:00' where id = 1001;
+UPDATE WEB_PAGE SET TIME_CREATED = '2024-02-15T15:23:00', TIME_LAST_UPDATED = '2024-02-15T15:23:00' where id = 1002;
 
 -- Insert sample tags for the web pages
-INSERT INTO "WEBPAGE_TAG" (WEBPAGE_ID, TAG_ID) VALUES (-1000, -1);
-INSERT INTO "WEBPAGE_TAG" (WEBPAGE_ID, TAG_ID) VALUES (-1000, -2);
-INSERT INTO "WEBPAGE_TAG" (WEBPAGE_ID, TAG_ID) VALUES (-1001, 1);
-INSERT INTO "WEBPAGE_TAG" (WEBPAGE_ID, TAG_ID) VALUES (-1002, 1);
-INSERT INTO "WEBPAGE_TAG" (WEBPAGE_ID, TAG_ID) VALUES (-1002, 3);
---INSERT INTO "WEBPAGE_TAG" (WEBPAGE_ID, TAG_ID) VALUES (-1001, -1);
---INSERT INTO "WEBPAGE_TAG" (WEBPAGE_ID, TAG_ID) VALUES (-1002, -3);
+INSERT INTO WEBPAGE_TAG (WEBPAGE_ID, TAG_ID) VALUES (1000, 1);
+INSERT INTO WEBPAGE_TAG (WEBPAGE_ID, TAG_ID) VALUES (1000, 2);
+INSERT INTO WEBPAGE_TAG (WEBPAGE_ID, TAG_ID) VALUES (1001, 1);
+INSERT INTO WEBPAGE_TAG (WEBPAGE_ID, TAG_ID) VALUES (1002, 1);
+INSERT INTO WEBPAGE_TAG (WEBPAGE_ID, TAG_ID) VALUES (1002, 3);
 
 -- Insert thumbnail
 
-UPDATE "WEB_PAGE" SET THUMBNAIL = 'img/thumbnails/memoriesOfMurderThumbnail.jpg' WHERE id = -1000;
-UPDATE "WEB_PAGE" SET THUMBNAIL = 'img/thumbnails/gameTest.png' WHERE id = -1001;
-UPDATE "WEB_PAGE" SET THUMBNAIL = 'img/thumbnails/wiiParty.png' where id = -1002;
+UPDATE WEB_PAGE SET THUMBNAIL = 'img/thumbnails/memoriesOfMurderThumbnail.jpg' WHERE id = 1000;
+UPDATE WEB_PAGE SET THUMBNAIL = 'img/thumbnails/gameTest.png' WHERE id = 1001;
+UPDATE WEB_PAGE SET THUMBNAIL = 'img/thumbnails/wiiParty.png' where id = 1002;
 
 --Insert Text of articles
 
-UPDATE "WEB_PAGE" SET ARTICLE_TEXT = '<v,1>"Memories of Murder" is a South Korean crime-drama film directed by Bong Joon-ho, released in 2003. [p] <l,>brahHello welcome to this article brahHello welcome to this article brah[p] <r,>Hello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brah' WHERE id = -1000;
-UPDATE "WEB_PAGE" SET ARTICLE_TEXT = '<v,0>Developing games using JFrame in Java provides a versatile platform for creating 2D graphical applications with user interaction.[p]<,>o this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brah' WHERE id = -1001;
-UPDATE "WEB_PAGE" SET ARTICLE_TEXT = 'Artificial intelligence (AI) pathfinding approaches are fundamental in various fields, including robotics, video games, logistics, and more. [p] <l,>These approaches aim to find the most efficient path from a starting point to a goal location, navigating through obstacles and constraints. Here are some common AI pathfinding approaches:test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test' WHERE id = -1002;
+--UPDATE WEB_PAGE SET ARTICLE_TEXT = '<v,1>"Memories of Murder" is a South Korean crime-drama film directed by Bong Joon-ho, released in 2003. [p] <l,>brahHello welcome to this article brahHello welcome to this article brah[p] <r,>Hello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brah' WHERE id = 1000;
+--UPDATE WEB_PAGE SET ARTICLE_TEXT = '<v,0>Developing games using JFrame in Java provides a versatile platform for creating 2D graphical applications with user interaction.[p]<,>o this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brahHello welcome to this article brah' WHERE id = 1001;
+--UPDATE WEB_PAGE SET ARTICLE_TEXT = 'Artificial intelligence (AI) pathfinding approaches are fundamental in various fields, including robotics, video games, logistics, and more. [p] <l,>These approaches aim to find the most efficient path from a starting point to a goal location, navigating through obstacles and constraints. Here are some common AI pathfinding approaches:test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test' WHERE id = 1002;
+UPDATE WEB_PAGE SET ARTICLE_TEXT = '<v,2>"Memories of Murder"' WHERE id = 1000;
+update WEB_PAGE SET ARTICLE_TEXT = '<v,0>Developing games using JFRAME' WHERE id =1001;
+update WEB_PAGE SET ARTICLE_TEXT = '<l,2> Artificial Intelligent (AI)' where id = 1002;
 
-INSERT INTO "VISUAL_SOURCE" (SOURCE_ID, IMAGE_PATH) VALUES (0,'img/thumbnails/memoriesOfMurderThumbnail.jpg');
-INSERT INTO "VISUAL_SOURCE" (SOURCE_ID, IMAGE_PATH) VALUES (1, 'img/thumbnails/gameTest.png');
+
+INSERT INTO VISUAL_SOURCE (SOURCE_ID, IMAGE_PATH) VALUES (0,'img/thumbnails/memoriesOfMurderThumbnail.jpg');
+INSERT INTO VISUAL_SOURCE (SOURCE_ID, IMAGE_PATH) VALUES (1, 'img/thumbnails/gameTest.png');
