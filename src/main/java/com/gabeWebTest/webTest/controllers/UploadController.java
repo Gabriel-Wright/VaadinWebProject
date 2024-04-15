d828c51b0e1252ad14e4314dade27ba1db55291b
Gabriel Wright <gabemagwright@gmail.com>
2024-04-15 18:49:06 +0100

    Handling upload fail-safes as best as I can. Need to check Tags table - that is causing issues particularly with color.

--- src/main/java/com/gabeWebTest/webTest/controllers/UploadController.java
+++ src/main/java/com/gabeWebTest/webTest/controllers/UploadController.java
@@ -7,7 +7,10 @@ import com.gabeWebTest.webTest.data.webPage.WebPageRepository;
 import com.gabeWebTest.webTest.data.webPage.tags.Tag;
 import com.gabeWebTest.webTest.data.webPage.tags.TagRepository;
 import com.gabeWebTest.webTest.services.WebPageService;
-import com.gabeWebTest.webTest.utils.ImageHandler;
+import com.gabeWebTest.webTest.utils.FileSaveHandler;
+import io.micrometer.common.util.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +20,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
+
+import static com.gabeWebTest.webTest.utils.FileSaveHandler.saveImageToDest;
+import static com.gabeWebTest.webTest.utils.FileSaveHandler.saveTextToDest;
 
 @RestController
 @RequestMapping("/api/v1/upload")
@@ -31,6 +39,10 @@ public class UploadController {
     private final TagRepository tagRepository;
     private final VisualSourceRepository visualSourceRepository;
 
+    private final String resourcesPath = "src/main/resources/static";
+
+    private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+
     public UploadController(WebPageService webPageService, WebPageRepository webPageRepository, TagRepository tagRepository, VisualSourceRepository visualSourceRepository) {
         this.webPageService = webPageService;
         this.webPageRepository = webPageRepository;
@@ -38,19 +50,39 @@ public class UploadController {
         this.visualSourceRepository = visualSourceRepository;
     }
 
-    @PostMapping("/images")
-    public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile imageFile) throws IOException {
-        //NEED TO HANDLE CHECKS BEFORE THIS BETTER e.g. file type - is it already saved there, stuff like that.
-
-        String fileName = imageFile.getOriginalFilename();
-        if(imageFile==null) {
+    @PostMapping("/text")
+    public ResponseEntity<String> uploadText(@RequestParam("image") MultipartFile textFile) throws IOException {
+        String fileName = textFile.getOriginalFilename();
+        if(StringUtils.isBlank(fileName)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No file presented");
         }
-        if(visualSourceRepository.findByImagePath(fileName).isPresent()) {
+        // Check if the content type is appropriate for an image
+        // Check if the content type is appropriate for a text file
+        if (!isTextContentType(textFile.getContentType())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid file type. Only text files are allowed.");
+        }
+        // Save the text file to the destination
+        saveTextToDest(textFile);
+        return ResponseEntity.ok("Text file:" + fileName + " uploaded successfully");
+    }
+
+    @PostMapping("/images")
+    public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile imageFile) throws IOException {
+        String fileName = imageFile.getOriginalFilename();
+        if(StringUtils.isBlank(fileName)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No file presented");
+        }
+        String testValue = "/img/" + fileName;
+        if(visualSourceRepository.findByImagePath("/img/" + fileName).isPresent()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Image already exists under path:"+fileName);
         }
-        ImageHandler.saveImageToDest(imageFile);
-        visualSourceRepository.save(new VisualSource("/img/"+fileName));
+        Optional<VisualSource> visualSource = visualSourceRepository.findByImagePath(testValue);
+        // Check if the content type is appropriate for an image
+        if (!isImageContentType(imageFile.getContentType())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid file type. Only image files are allowed.");
+        }
+        saveImageToDest(imageFile);
+        visualSourceRepository.save(new VisualSource("/img/" + fileName));
         return ResponseEntity.ok("Image:" + fileName+" uploaded successfully");
     }
 
@@ -60,11 +92,19 @@ public class UploadController {
                                               @RequestParam("thumbnail_path") String thumbnail,
                                               @RequestParam("article_preview_text") String previewText,
                                               @RequestParam("tags") Set<String> tags) {
+        if(StringUtils.isBlank(title)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("WebPage title cannot be blank");
+        }
         if(webPageIsPresent(title)) {
             // Return a response indicating that the webpage already exists
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Webpage with title '" + title + "' already exists");
         }
-
+        if(!isValidSavedImagePath(thumbnail)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No thumbnail under that path found");
+        }
+        if(!isValidTextFilePath(articleTextPath)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No txt file under that path found");
+        }
         // Proceed with creating and saving the new webpage
         WebPage uploadArticle = new WebPage(title, articleTextPath, thumbnail, previewText, resolveTags(tags));
         webPageRepository.save(uploadArticle);
@@ -90,5 +130,36 @@ public class UploadController {
 
     private boolean webPageIsPresent(String title) {
         return webPageRepository.findByTitle(title).isPresent();
+    }
+
+    private boolean isImageContentType(String contentType) {
+        return contentType != null && (contentType.startsWith("image/jpeg") || contentType.startsWith("image/png"));
+    }
+
+    private boolean isTextContentType(String contentType) {
+        // Add additional checks if needed
+        return contentType != null && contentType.toLowerCase().startsWith("text/");
+    }
+
+    private boolean isValidSavedImagePath(String imagePath) {
+        // If the image path given is blank or doesn't end with either .png or .jpg, then return false
+        if (StringUtils.isBlank(imagePath) || !((imagePath.toLowerCase().endsWith(".png") || imagePath.toLowerCase().endsWith(".jpg")))) {
+            return false;
+        }
+
+        // Check if the file exists
+        File file = new File(resourcesPath + imagePath);
+        return file.exists();
+    }
+
+    private boolean isValidTextFilePath(String textFilePath) {
+        // If the textFile provided is blank or doesn't end with .txt, then return false
+        if (StringUtils.isBlank(textFilePath) || !textFilePath.toLowerCase().endsWith(".txt")) {
+            return false;
+        }
+
+        // Check if the file exists
+        File file = new File(resourcesPath + textFilePath);
+        return file.exists();
     }
 }
--- src/main/java/com/gabeWebTest/webTest/utils/FileSaveHandler.java
+++ src/main/java/com/gabeWebTest/webTest/utils/FileSaveHandler.java
@@ -7,9 +7,10 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class ImageHandler {
+public class FileSaveHandler {
 
     private static final String IMAGE_DESTINATION = "src/main/resources/static/img/";
+    private static final String TEXT_DESTINATION = "src/main/resources/static/text/";
 
     public static void saveImageToDest(MultipartFile file) throws IOException {
         // Create the directory if it doesn't exist
@@ -18,6 +19,19 @@ public class ImageHandler {
         // Copy the input stream of the uploaded file to the destination file
         try (InputStream inputStream = file.getInputStream();
              OutputStream outputStream = new FileOutputStream(new File(IMAGE_DESTINATION + file.getOriginalFilename()))) {
+            FileCopyUtils.copy(inputStream, outputStream);
+        } catch (IOException e) {
+            throw new IOException("Failed to save image to destination", e);
+        }
+    }
+
+    public static void saveTextToDest(MultipartFile file) throws IOException {
+        // Create the directory if it doesn't exist
+        Files.createDirectories(Paths.get(TEXT_DESTINATION));
+
+        // Copy the input stream of the uploaded file to the destination file
+        try (InputStream inputStream = file.getInputStream();
+             OutputStream outputStream = new FileOutputStream(new File(TEXT_DESTINATION + file.getOriginalFilename()))) {
             FileCopyUtils.copy(inputStream, outputStream);
         } catch (IOException e) {
             throw new IOException("Failed to save image to destination", e);
